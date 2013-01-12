e6f4c79769b7a991b0f66404887cd27f0d1797b7
Dan Skorupski <dan.skorupski@gmail.com>
2013-01-12 16:22:01 -0500

    Properly escape slashes in EscapeString.

--- src/common.cpp
+++ src/common.cpp
@@ -19,6 +19,8 @@ void EscapeString(const string& str, ostream& os)
   for(size_t i = 0; i < str.size(); i++) {
     if(str[i] == '\"')
       os << "\\\"";
+    else if(str[i] == '\\')
+      os << "\\\\";
     else if(!isprint(str[i]))
       os << "\\x" << hex << setw(2) << setfill('0') << (int)str[i];
     else
